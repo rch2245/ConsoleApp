262ca6d1f81ace8f08e91bca26959f1df06ec108
RAJA <RAJA@DESKTOP-SDVCBA5>
2025-06-16 11:31:11 -0400

    Add project files.

---
+++ ConsoleApp.cpp
@@ -0,0 +1,17 @@
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char first, middle, last;
+
+    first = 'R';
+    middle = 'C';
+    last = 'H';
+    cout << first << middle << last << endl;
+    return 0;
+}
+
